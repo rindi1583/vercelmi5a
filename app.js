6ccd043f783b80b2edc23cfc8f279076c9d07e12
rindi1583 <rndi@mhs.mdp.ac.id>
2023-10-28 14:29:23 +0700

    2

--- app.js
+++ app.js
@@ -5,24 +5,28 @@ const app = express()
 const mongoose = require('mongoose')
 
 require('dotenv/config')
+const cors = require ('cors')
 
 // 6. bodyparser
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json())
+app.use(cors())
 
 // import routes
 const fakultasRoutes = require('./routes/fakultas')
 const prodiRoutes = require('./routes/prodi')
 const dosenRoutes = require('./routes/dosen')
 const mahasiswaRoutes = require('./routes/mahasiswa')
+const authRoutes = require('./routes/auth')
 
 // 8. put routes in express
 app.use('/fakultas', fakultasRoutes)
 app.use('/prodi', prodiRoutes)
 app.use('/dosen', dosenRoutes)
 app.use('/mahasiswa', mahasiswaRoutes)
+app.use('/auth', authRoutes)
 // 3. connect to database
 mongoose.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true, useUnifiedTopology: true
